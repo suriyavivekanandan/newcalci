20ebee578f3905df9b529f9b9923cb52d156bb86
suriyavivekanandan <130919330+suriyavivekanandan@users.noreply.github.com>
2024-10-07 15:21:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const display=document.getElementById("display")
+function appendToDisplay(input){
+    display.value=display.value+input
+
+}
+function clearDisplay(){
+ display.value=""
+}
+function calculate(){
+    try{
+        display.value=eval(display.value)
+    }
+    catch(error){
+         display.value="Error"
+    }
+    
+}
+function DelDisplay(){
+    display.value= display.value.slice(0,-1)
+}
